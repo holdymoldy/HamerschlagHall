4e82757af1b4aa158fe659aa43c6ce31be518d5d
Holden Parks <holden@Holdens-MBP.wv.cc.cmu.edu>
2017-11-18 14:03:20 -0500

    setter for paper and personality, header

---
+++ Paper_and_personality.h
@@ -0,0 +1,41 @@
+
+#ifndef PapPer_h
+#define PapPer_h
+
+
+class Paper{
+public:
+    Paper();
+    ~Paper();
+    char* journal;
+    char* title;
+    int generateCitations(float intelligence, int focus, float risk);
+    int SubmitPaper(float intelligence, int focus, float risk);
+    void GeneratePaperAfterSuccess(float intelligence, int focus, float risk);
+    int getCitations();
+    
+private:
+    int citations;
+
+};
+
+class Personality{
+public:
+    Personality();
+    ~Personality();
+    void generatePersonality(float knowledge, float prestige, float mentoring);
+    float getIntelligence();
+    float getOptimism();
+    float getStamina();
+    void setIntelligence(float i);
+    void setOptimism(float o);
+    void setStamina(float s);
+    
+private:
+    float intelligence;
+    float optimism;
+    float stamina;
+};
+
+
+#endif
