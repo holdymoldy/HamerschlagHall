f32c371ecacf2b16180df1dda02f7d73b7ec001b
ALIALP <aalp.gurer@gmail.com>
2017-11-30 17:00:33 -0500

    pull holden and push holden

--- Main.cpp
+++ Main.cpp
@@ -551,7 +551,7 @@ int main(void)
 		people[i].g_skin = 205;
 		people[i].b_skin = 148;
 		people[i].r_hair = i*10+rand()%80;
-		people[i].g_hair = i*30;
+		people[i].g_hair = i*10+rand()%150;
 		people[i].b_hair = rand()%255;
 		people[i].r_shirt = 192;
 		people[i].g_shirt = 192;
@@ -981,7 +981,10 @@ int main(void)
         
         window_term = 0;
         inter_state = 0;
-        // ADD OTHER UPDATES AT END OF A SEMESTER HERE
+		int RandomEventlikelyHood = rand() % 2;
+		if (RandomEventlikelyHood == 1) {
+			ADV.RandomEvents(Student, StudentCounter);
+		}
 		for (int i = 0; i < nDesk; i += 1){
 			if (people[i + 1].state_person == 1) {
 				
