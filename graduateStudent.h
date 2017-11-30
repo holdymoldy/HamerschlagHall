4c92d531fdf22882a06406ecc37cb41ca54e00d1
Adarsh Dave <adarsh.r.dave@PCLOADLETTER.wv.cc.cmu.edu>
2017-11-30 17:57:57 -0500

    Coloring functions

--- graduateStudent.h
+++ graduateStudent.h
@@ -21,13 +21,12 @@ class GradStudent {
 protected:
     
     int year;
-    int *color;
+    int *haircolor,*eyecolor,*skincolor;
     Counter *happiness,*rout;
     int focus;
     
 public:
     GradStudent(float knowledge, float prestige, float mentoring);
-	GradStudent();
     //    GradStudent* copy();
     GradStudent(const GradStudent &from);
     GradStudent &operator=(const GradStudent &from);
@@ -50,20 +49,25 @@ public:
     void ModifyResearchValue(float amount);
     void ModifyResearchdT(float amount);
     void ModifyResearchBasedT(float amount);
-	float GetResearchVal();
-	float GetHappinessVal();
     void CreatePaper();
-    void GeneratePaperAfterSuccess(); //after success
-    void NameStudent(char *desired, int desiredlength);
+    int SubmitPaper(float risk); //1 if successful, paper returned, out returned to zero, then currpaper is null. 0 if not successful.
+    void GeneratePaperAfterSuccess(float risk); //after success
+    void NameStudent(char desired[], int desiredlength);
     void PrintName();
     void PrintHappiness();
-    void SetColor(int arr[]);
-    void GetColor(int result[]);
+    void SetHairColor(int arr[]);
+    void GetHairColor(int result[]);
+    void SetEyeColor(int arr[]);
+    void GetEyeColor(int result[]);
+    void SetSkinColor(int arr[]);
+    void GetSkinColor(int result[]);
     int GetYear();
     int GetFocus();
     void turn();
     void GenerateName();
-    void GenerateColor();
+    void GenerateHairColor();
+    void GenerateEyeColor();
+    void GenerateSkinColor();
 };
 
 
